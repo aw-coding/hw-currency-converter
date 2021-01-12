0606260f8160881f58f8294996542bf8600b0ca0
Andrew Wood <andrewwood90@hotmail.com>
2021-01-12 17:46:23 +0000

    accessing api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
+
+
 import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el:"#app"
+        el:"#app",
+        data: {
+            allCurrencies: [],
+            currencyFrom: '',
+            currencyTo: '',
+        },
+        mounted(){
+            this.getCurrencies()
+        },
+        methods: {
+            getCurrencies: function() {
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(response => response.json())
+                .then((data) => this.allCurrencies = data)
+
+            },
+
+        },
     })
 })
+
+
+
